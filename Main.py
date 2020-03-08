8b6326caf49e3f682ab871733bbdf9cdcc226be3
Galdino <mateusgaldino1000@hotmail.com>
2020-03-08 12:14:33 -0300

    Python Calculator and Exceptions treatment

---
+++ Main.py
@@ -0,0 +1,46 @@
+import re
+
+print("Our magical calculator")
+print("Type new to reset the calculator")
+print("Type quit to exit")
+
+previous = 0
+run = True
+
+
+def perform_math():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
+
+    safe_input = previous
+
+    if equation == 'quit':
+        run = False
+        print("Exiting calculator")
+    elif equation == 'new':
+        previous = 0
+    else:
+        equation = re.sub('[a-zA-z,.:()" "|^~´{}@#$%¨&!]', "", equation)
+        try:
+            if previous == 0:
+                previous = eval(equation)
+            else:
+                previous = eval(str(previous) + equation)
+        except SyntaxError:
+            print("Unexpected character or end of line in equation. Please, try again")
+            previous = safe_input
+
+
+try:
+    while run:
+        perform_math()
+except SyntaxError:
+    print("Unexpected character or end of line. Please, try again")
+finally:
+    while run:
+        perform_math()
